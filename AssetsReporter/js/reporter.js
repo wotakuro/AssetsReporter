9bb4691d117eed91f76094399553e84f4907e697
wotakuro <wotakuro@gmail.com>
2016-04-20 15:05:18 +0900

    検索条件追加パス名含む含まないの指定

--- AssetsReporter/js/reporter.js
+++ AssetsReporter/js/reporter.js
@@ -29,6 +29,42 @@ function setupCheckBox( data,thStr ,headStr,writeTo ){
   $(writeTo).html(html);
 }
 
+function getConditionList( idstr ){
+   var list = $("#" +idstr + " input.cond_value");
+   var idx = 0;
+   var ret = [];
+   list.each( function(){
+     var tmp = $(this).val();
+     if( tmp ){
+       ret[idx] = tmp; ++ idx;
+     }
+   } );
+
+   return ret;
+}
+
+function isIncludeFileName( path , cond_list ){
+   if( !cond_list || cond_list.length == 0 ){ return true; }
+   var length = cond_list.length;
+   for( var i = 0; i < length; ++ i ){
+     if( path.indexOf(cond_list[i] ) >= 0 ){
+       return true;
+     }
+   }
+   return false;
+}
+
+function isExcludeFileName( path , cond_list ){
+   if( !cond_list || cond_list.length == 0 ){ return false; }
+   var length = cond_list.length;
+   for( var i = 0; i < length; ++ i ){
+     if( path.indexOf(cond_list[i] ) >= 0 ){
+       return true;
+     }
+   }
+   return false;
+}
+
 $(document).ready( function(){
   var date_str = "report : " + g_report_at;
   var platform_str = g_report_platform;
@@ -41,3 +77,13 @@ $(document).ready( function(){
   $("#report_at").html(date_str);
   $("#platform").html(platform_str);
 });
+
+$(document).on("click",".add_cond",function() {
+  var str = '<div><input class="cond_value" type="text" size="80"><input type="button" class="del_cond" value="íœ"></div>';
+  $(this).before(str);
+});
+$(document).on("click",".del_cond",function() {
+  $(this).parent('div').remove();
+});
+
+
