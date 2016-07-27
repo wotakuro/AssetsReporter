9cc79cb56bb0d1347403feb14c580dc2e8e6d4c4
wotakuro <wotakuro@gmail.com>
2016-07-28 02:03:17 +0900

    ab multi-lang

---
+++ AssetsReporter/template/en/template_ab.js
@@ -0,0 +1,43 @@
+g_en_template_ab = {
+	// common
+	tplNoPlatform			: "none",
+	tplWarning				: "Warning",
+	tplSearchFilter			: "SearchFilter",
+	tplSearchTitle			: "Search",
+	tplSearchBtn			: "Search",
+	tplSearchResultTitle	: "Search Result",
+	tplCheckNon				: "No Check",
+	tplCheckEnableOnly		: "Enable Only",
+	tplCheckDisableOnly		: "Disable Only",
+	tplHitNum				: "HitNum",
+	tplItem					: "Item",
+	tplCheck				: "Check",
+	tplCondition			: "Condition",
+	tplAddConditionBtn		: "Add condition",
+	tplDelConditionBtn		: "delete",
+	tplFileInclude			: "If filepath includes below words",
+	tplFileExclude			: "If filepath doesn't include below words",
+
+	// specific
+	tplCondIncludeDepends	: "Include / Depends file",
+	tplCondIncludeFiles		: "AssetBundle includes below files",
+	tplCondDependsFiles		: "AssetBundle depends on below files",
+
+	tplCondAssetBundleName	: "Asset BundleName",
+	tplCondIncludeAbName	: "If AssetBundle's name include below words",
+	tplCondExcludeAbName	: "If AssetBundle's name doesen't include below words ",
+	
+	// result
+	tplResultFileList			: "FileList",
+	tplResultFileDependencies	: "Dependencies",
+	tplResultAbDependencies		: "Dependencies",
+	tplResultType				: "Type",
+	tplResultPath				: "Path",
+	tplResultBundleName			: "bundle name",
+	tplResultBundleValiant		: "bundle valiant",
+	tplResultNoDependencies		: "No dependencies",
+	tplResultListen				: "Listen",
+
+	tplResultDuplicateFiles		: "DuplicateFiles"
+
+};
