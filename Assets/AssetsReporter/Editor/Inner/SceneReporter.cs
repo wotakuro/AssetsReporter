6ba69bd348edc9839a2e63f23825ef5c06faaf01
wotakuro <wotakuro@gmail.com>
2017-07-08 09:39:41 +0900

    refactor & scene reporter

--- Assets/AssetsReporter/Editor/Inner/SceneReporter.cs
+++ Assets/AssetsReporter/Editor/Inner/SceneReporter.cs
@@ -9,8 +9,6 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 
 public class SceneReporter {
-
-
     private List<string> GetBuildScenesList()
     {
         List<string> buildScenes = new List<string>();
@@ -23,7 +21,7 @@ public class SceneReporter {
         return buildScenes;
     }
 
-    public static void CreateReport(string pl, List<string> exList)
+    public static void CreateReport()
     {
         var reporter = new SceneReporter();
         reporter.ReportScenes(AssetsReporterUtils.ResultDir + "report_scene.js");
@@ -55,31 +53,79 @@ public class SceneReporter {
         }
         catch (Exception e)
         {
-
+            Debug.LogError(e);
         }
     }
     private void ReportSceneAtPath( StringBuilder sb,string scenePath)
     {
         sb.Append("{");
-        AssetsReporterUtils.AddJsonObject(sb, "scenePath", scenePath ).Append(",");
-        var scene = EditorSceneManager.OpenScene(scenePath);
+        AssetsReporterUtils.AddJsonObject(sb, "scenePath", scenePath ).Append(",\n");
 
+        ReportSceneDependAssets(sb, scenePath);
+        sb.Append(",");
+
+        var scene = EditorSceneManager.OpenScene(scenePath);
         var rootObjects = scene.GetRootGameObjects();
 
+        int allGameObjectCount = 0;
+        int allComponentCount = 0;
+        int allMonoBehaviourCount = 0;
+        foreach (var rootObj in rootObjects)
+        {
+            allGameObjectCount += CountChildTransform(rootObj.transform);
+            var childComponents = rootObj.GetComponentsInChildren<Component>();
+            var childMonoBehaviour = rootObj.GetComponentsInChildren<MonoBehaviour>();
+            if (childComponents != null) { allComponentCount += childComponents.Length; }
+            if (childMonoBehaviour != null) { allMonoBehaviourCount += childMonoBehaviour.Length; }
+        }
+        AssetsReporterUtils.AddJsonObject(sb, "allGameObjects", allGameObjectCount).Append(",");
+        AssetsReporterUtils.AddJsonObject(sb, "allComponents", allComponentCount).Append(",");
+        AssetsReporterUtils.AddJsonObject(sb, "allMonoBehaviour", allMonoBehaviourCount);
 
         sb.Append("}");
     }
 
-    private void ReportSceneDependAssets(StringBuilder sb , string scenePath){
+    private int CountChildTransform(Transform trans)
+    {
+        if(trans.childCount == 0 ){
+            return 1;
+        }
+        int num = 0;
+        foreach (Transform child in trans)
+        {
+            num = num + CountChildTransform( child  );
+        }
+        return num;
+    }
+
+    private StringBuilder ReportSceneDependAssets(StringBuilder sb , string scenePath){
         var depends = AssetDatabase.GetDependencies(scenePath, true);
         sb.Append("depends:[");
         if (depends != null) {
+            bool firstFlag = true;
             foreach (var depend in depends)
             {
                 if (depend == null) { continue; }
+                if (firstFlag) { firstFlag = false; }
+                else { sb.Append(','); }
+                var importer = AssetImporter.GetAtPath(depend);
+                var obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(depend);
+                sb.Append("{");
+                AssetsReporterUtils.AddJsonObject(sb, "path", depend).Append(",");
+                AssetsReporterUtils.AddJsonObject(sb, "importer", importer.GetType().ToString());
+
+                if (obj != null)
+                {
+                    sb.Append(",");
+                    AssetsReporterUtils.AddJsonObject(sb, "type", obj.GetType().ToString());
+                }
+                obj = null;
+                sb.Append("}\n");
             }
-        }       
-        sb.Append("]");
+        }
+        Resources.UnloadUnusedAssets();
+        sb.Append("]\n");
+        return sb;
     }
 
 
