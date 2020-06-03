9da74d31a6484b26370310a90a28159ec924ec7f
Yusuke Kurokawa <wotakuro@gmail.com>
2020-06-04 00:18:54 +0900

    update

--- Editor/Inner/AssetsReporterUtils.cs
+++ Editor/Inner/AssetsReporterUtils.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.iOS;
+//using UnityEditor.iOS;
 #if UNITY_2017_3_OR_NEWER
 using UnityEngine.Rendering;
 #endif
